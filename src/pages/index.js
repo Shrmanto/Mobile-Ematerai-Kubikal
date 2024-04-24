ec861eb385b1e6a8ba25377e1d034e92f16ceaff
RendyRensFirmansyah <rendyfirmansyah.job@gmail.com>
2024-04-25 00:31:03 +0700

    feat: Navigation Bar

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import Dokumen from "./Dokumen";
+import Kontak from "./Kontak";
+import Notification from "./Notification";
+
+export {
+    Home,
+    Dokumen,
+    Kontak,
+    Notification
+}
