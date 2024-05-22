e815e86ec3e031eeab057ac577a2ff91372104cf
RendyRensFirmansyah <rendyfirmansyah.job@gmail.com>
2024-05-23 04:28:32 +0700

    feat: draggable materai

--- src/pages/Document/DetailDoc.js
+++ src/pages/Document/DetailDoc.js
@@ -4,20 +4,36 @@ import {
   View,
   Dimensions,
   TouchableOpacity,
+  Animated,
+  TouchableWithoutFeedback,
+  Modal,
   ScrollView,
+  Touchable,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Feather, AntDesign, EvilIcons, MaterialIcons, Octicons } from '@expo/vector-icons';
+import {
+  Feather,
+  AntDesign,
+  EvilIcons,
+  MaterialIcons,
+  Octicons,
+  MaterialCommunityIcons,
+  FontAwesome5,
+} from "@expo/vector-icons";
 import Pdf from "react-native-pdf";
+import Draggable from "react-native-draggable";
 import { primaryColor, secondaryColor } from "../../components/Color";
+
+const Materai = require("../../assets/e-Materai.png");
 
 export default function DetailDoc({ navigation }) {
   //   const navigation = useNavigation();
   const [SelectFile, setSelectFile] = useState();
   const [numberOfPages, setNumberOfPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
+  const [MateraiImages, setMateraiImages] = useState(null);
 
   const getFile = async () => {
     const files = await AsyncStorage.getItem("document");
@@ -45,21 +61,67 @@ export default function DetailDoc({ navigation }) {
     }
   };
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [slideAnim] = useState(new Animated.Value(0));
+
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    Animated.timing(slideAnim, {
+      toValue: 0,
+      duration: 100,
+      useNativeDriver: true,
+    }).start(() => setIsModalVisible(false));
+  };
+
+  useEffect(() => {
+    if (isModalVisible) {
+      Animated.timing(slideAnim, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      slideAnim.setValue(0);
+    }
+  }, [isModalVisible]);
+
+  const addMaterai = () => {
+    if (!MateraiImages) {
+      setMateraiImages(Materai);
+    }
+  };
+
+  const removeImage = () => {
+    if (MateraiImages) {
+      setMateraiImages(null);
+    }
+  };
+
   return (
     <View style={styles.container}>
-        <View style={{paddingHorizontal:16, alignItems:'center', justifyContent:"space-between", flexDirection:"row", height:50, backgroundColor:"#fff"}}>
-          <TouchableOpacity onPress={() => navigation.navigate('Navigate')}>
-              <AntDesign name="close" size={24} color="black" />
-          </TouchableOpacity>
-          <Text></Text>
-          <View>
-            <Text>Lanjutkan</Text>
-          </View>
+      <View
+        style={{
+          paddingHorizontal: 16,
+          alignItems: "center",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          height: 50,
+          backgroundColor: "#fff",
+        }}
+      >
+        <TouchableOpacity onPress={() => navigation.navigate("Navigate")}>
+          <AntDesign name="close" size={24} color="black" />
+        </TouchableOpacity>
+        <Text></Text>
+        <View>
+          <Text>Lanjutkan</Text>
         </View>
+      </View>
       {SelectFile && (
-        <ScrollView
-        showsVerticalScrollIndicator={false}
-        >
+        <ScrollView showsVerticalScrollIndicator={false}>
           <Pdf
             source={{ uri: SelectFile.uri }}
             onLoadComplete={(numberOfPages, filePath) => {
@@ -75,45 +137,240 @@ export default function DetailDoc({ navigation }) {
             }}
             page={currentPage}
             scale={1.0}
-            enablePaging={true}
+            enableDoubleTapZoom={true}
             style={styles.pdf}
           />
-          {/* <Text>File path : {SelectFile.uri} </Text>
-          <Text>Name File : {SelectFile.name} </Text>
-          <Text>Size File : {SelectFile.size} </Text> */}
         </ScrollView>
       )}
-      <View style={{position:"absolute", bottom:0, backgroundColor:"#fff", width:'100%', maxWidth:392, height:150, paddingHorizontal:16, paddingVertical:20}}>
-        <View style={{flexDirection:"row", justifyContent:"space-between"}}>
-          <View style={{flexDirection:"row"}}>
-            <View style={{width:30, height:30, borderColor:"#757575", borderWidth:1, borderRadius:5, alignItems:"center", justifyContent:"center"}}>
+      {MateraiImages && (
+        <Draggable
+          imageSource={Materai}
+          renderSize={50}
+          x={0}
+          y={0}
+          z={1}
+          onDragRelease={() => console.log("Image dragged")}
+          onLongPress={() => console.log("log press")}
+          onShortPressRelease={() => console.log("press drag")}
+          onPressIn={() => console.log("in press")}
+          onPressOut={() => console.log("out press")}
+        />
+      )}
+      <View
+        style={{
+          position: "absolute",
+          bottom: 0,
+          backgroundColor: "#fff",
+          width: "100%",
+          maxWidth: 392,
+          height: 150,
+          paddingHorizontal: 16,
+          paddingVertical: 20,
+        }}
+      >
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <View style={{ flexDirection: "row" }}>
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                borderColor: "#757575",
+                borderWidth: 1,
+                borderRadius: 5,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <Feather name="zoom-in" size={20} color="black" />
             </View>
-            <View style={{width:30, height:30, marginLeft:15, borderColor:"#757575", borderWidth:1, borderRadius:5, alignItems:"center", justifyContent:"center"}}>
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                marginLeft: 15,
+                borderColor: "#757575",
+                borderWidth: 1,
+                borderRadius: 5,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <Feather name="zoom-out" size={20} color="black" />
             </View>
           </View>
-          <View style={{flexDirection:"row"}}>
-            <View style={{width:50, height:30, borderColor:"#757575", borderWidth:1, borderRadius:5, alignItems:"center", justifyContent:"center"}}>
+          <View style={{ flexDirection: "row" }}>
+            <View
+              style={{
+                width: 50,
+                height: 30,
+                borderColor: "#757575",
+                borderWidth: 1,
+                borderRadius: 5,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <Text>{currentPage}</Text>
             </View>
-            <View style={{marginLeft:15 ,alignItems:"center", justifyContent:"center"}}>
+            <View
+              style={{
+                marginLeft: 15,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <Text>/{numberOfPages}</Text>
             </View>
           </View>
-          <View style={{flexDirection:"row"}}>
-            <TouchableOpacity onPress={goToPreviousPage} style={{width:30, height:30, borderColor:"#757575", borderWidth:1, borderRadius:50, justifyContent:"center", alignItems:"center"}}>
+          {/* Modal */}
+          <Modal transparent={true} visible={isModalVisible}>
+            <TouchableWithoutFeedback onPress={closeModal}>
+              <View style={styles.modalOverlay}>
+                <Animated.View
+                  style={[
+                    styles.modalContent,
+                    {
+                      transform: [
+                        {
+                          translateY: slideAnim.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [300, 0],
+                          }),
+                        },
+                      ],
+                    },
+                  ]}
+                >
+                  <View
+                    style={{
+                      backgroundColor: "#fff",
+                      elevation: 1,
+                    }}
+                  >
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        paddingVertical: 15,
+                        paddingHorizontal: 20,
+                      }}
+                    >
+                      <Text style={{ fontSize: 18 }}>Pilih Token</Text>
+                      <TouchableWithoutFeedback onPress={closeModal}>
+                        <AntDesign name="closesquare" size={24} color="grey" />
+                      </TouchableWithoutFeedback>
+                    </View>
+                  </View>
+                  <View
+                    style={{
+                      paddingHorizontal: 16,
+                      marginTop: 20,
+                      alignItems: "center",
+                    }}
+                  >
+                    <TouchableWithoutFeedback onPress={addMaterai}>
+                      <View style={styles.cardToken}>
+                        <View
+                          style={{
+                            flexDirection: "row",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                        >
+                          <MaterialCommunityIcons
+                            name="postage-stamp"
+                            size={50}
+                            color="orange"
+                            style={styles.stampIcon}
+                          />
+                          <View>
+                            <Text style={{ fontSize: 18, fontWeight: 800 }}>
+                              e-Materai
+                            </Text>
+                            <Text
+                              style={{
+                                fontSize: 14,
+                                fontWeight: 300,
+                                opacity: 0.6,
+                              }}
+                            >
+                              Materai elektronik yang sah dari PERURI
+                            </Text>
+                          </View>
+                        </View>
+                      </View>
+                    </TouchableWithoutFeedback>
+                  </View>
+                </Animated.View>
+              </View>
+            </TouchableWithoutFeedback>
+          </Modal>
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity
+              onPress={goToPreviousPage}
+              style={{
+                width: 30,
+                height: 30,
+                borderColor: "#757575",
+                borderWidth: 1,
+                borderRadius: 50,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <MaterialIcons name="arrow-back-ios" size={20} color="black" />
             </TouchableOpacity>
-            <TouchableOpacity onPress={goToNextPage} style={{width:30, height:30, marginLeft:15, borderColor:"#757575", borderWidth:1, borderRadius:50, alignItems:"center", justifyContent:"center"}}>
+            <TouchableOpacity
+              onPress={goToNextPage}
+              style={{
+                width: 30,
+                height: 30,
+                marginLeft: 15,
+                borderColor: "#757575",
+                borderWidth: 1,
+                borderRadius: 50,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <MaterialIcons name="arrow-forward-ios" size={20} color="black" />
             </TouchableOpacity>
           </View>
         </View>
-        <View style={{marginTop:20, marginLeft:"auto"}}>
-          <View style={{width:50, height:50, backgroundColor:primaryColor, alignItems:"center", justifyContent:"center", borderRadius:50}}>
+        <View
+          style={{
+            marginTop: 20,
+            justifyContent: "space-between",
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            onPress={removeImage}
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: primaryColor,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 50,
+            }}
+          >
+            <FontAwesome5 name="trash" size={18} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={openModal}
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: primaryColor,
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: 50,
+            }}
+          >
             <MaterialIcons name="mode-edit" size={18} color="white" />
-          </View>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -125,13 +382,51 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 30,
   },
-  pdfContainer: {
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
-  },
   pdf: {
+    marginTop: 10,
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
+  },
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    width: "100%",
+    maxWidth: 390,
+    height: 550,
+    position: "absolute",
+    bottom: 0,
+    // borderTopRightRadius: 15,
+    // borderTopLeftRadius: 15,
+  },
+  barMenu: {
+    width: "auto",
+    marginHorizontal: 20,
+    height: 60,
+    borderBottomWidth: 1,
+    justifyContent: "center",
+    borderBottomColor: "rgba(0, 0, 0, 0.1)",
+  },
+  barMenuBottom: {
+    width: "auto",
+    marginHorizontal: 20,
+    height: 60,
+    justifyContent: "center",
+  },
+  cardToken: {
+    width: 360,
+    height: 90,
+    // paddingVertical: 30,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    elevation: 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  stampIcon: {
+    marginRight: 30,
   },
 });
